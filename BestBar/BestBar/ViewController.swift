79b475b2f97a19ea2f18dd11d7e72aeccb8aadd7
Chris McLearnon <chris@belfastlabs.com>
2019-08-23 11:21:32 +0100

    Initial Commit

---
+++ BestBar/BestBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BestBar
+//
+//  Created by Chris McLearnon on 23/08/2019.
+//  Copyright © 2019 BelfastLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
